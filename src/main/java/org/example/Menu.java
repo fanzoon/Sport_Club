eaada7c9417e92761e7d909a4594fa177b53bb30
ALIAKSEI RAIKO <alexeyraiko644@gmail.com>
2023-02-15 10:45:47 +0300

    Initial commit

---
+++ src/main/java/org/example/Menu.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void menu (){
+        int menu;
+        Scanner scanner = new Scanner(System.in);
+        startMenu();
+        do {
+            menu = scanner.nextInt();
+            switch (menu) {
+                case 0 -> startMenu();
+                case 1 -> {
+
+                }
+                case 2 -> {
+                }
+                case 3 -> System.out.println("Вы нажали 3");
+                case 9 -> System.out.println("Выход из программы");
+                default -> System.out.println("Выбрали несуществующий пункт меню");
+            }
+        } while (menu != 9);
+    }
+
+    private static void startMenu() {
+        System.out.println("Меню программы:");
+        System.out.println("Нажми 0, чтобы Вернуться в главное меню:");
+        System.out.println("Нажми 1, чтобы получить всех спортсменов");
+        System.out.println("Нажми 2, чтобы добавить спортсмена");
+        System.out.println("Нажми 3, чтобы удалить спортсмена");
+        System.out.println("Нажми 9, чтобы выйти из приложения");
+    }
+}
